fe4c6ebeccb670d0659460a453bffa5e4a7e33ad
Caio <caiofortesprofissional@gmal.com>
2024-09-18 01:38:25 -0300

    first hello world, cleaning project

---
+++ project/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script>
+    export default {
+        data(){
+            return{
+                teste: 'hello world'
+            }
+        }
+    }
+</script>
+
+<template>
+    {{ teste }}
+</template>
+
+<script setup>
+
+</script>
