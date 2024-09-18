6a28cdcb667386230153df0ad23531e4f719494f
Caio <caiofortesprofissional@gmal.com>
2024-09-18 02:52:03 -0300

    organizando css e fonts

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 
-
 </script>
 
 <template>
   <TheHeader />
-
 </template>
--- project/src/components/TheHeader.vue
+++ project/src/components/TheHeader.vue
@@ -1,15 +1,18 @@
 <script>
     export default {
         data(){
-            return{
-                teste: 'hello world'
-            }
+            return{}
         }
     }
 </script>
 
 <template>
-    {{ teste }}
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Proin imperdiet dolor mi, id pretium risus ullamcorper at. 
+    Cras ultricies urna in libero ultrices vehicula. Mauris nulla risus,
+    tincidunt eu nibh euismod, fermentum maximus magna. Donec eget libero luctus, 
+    tincidunt nibh a, semper tortor. Aliquam vestibulum mattis tempor.
+    Maecenas efficitur malesuada risus sit amet semper. Fusce quis erat ipsum. 
 </template>
 
 <script setup>
