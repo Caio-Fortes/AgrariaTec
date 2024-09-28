88e7685ff828ed6ee85834cf81c2f8ee2a65e0e8
Caio <caiofortesprofissional@gmal.com>
2024-09-28 14:18:51 -0300

    finalizando prgramação do evento

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,10 +2,12 @@
 import TheHeader from './components/TheHeader.vue';
 import Capa from './components/Capa.vue';
 import QuemSomos from './components/QuemSomos.vue';
+import ProgramacaoEvento from './components/Programacao.vue';
 </script>
 
 <template>
   <TheHeader />
   <Capa />
   <QuemSomos />
+  <ProgramacaoEvento />
 </template>
