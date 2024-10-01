069d862db48d93c11b04f7f693082dd0e39511d4
Caio <caiofortesprofissional@gmal.com>
2024-09-30 21:39:14 -0300

    footer

--- project/src/components/Contato.vue
+++ project/src/components/Contato.vue
@@ -1,2 +1,23 @@
 <template>
+    <div class="flex">
+        <div class="footer-logo-container">
+            <div class="logo-container-footer">
+                <img src="/images/logo.png" alt="AgráriaTEC Logo" />
+            </div>
+            <div class="description-footer">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin  semper tortor. 
+                </p> 
+            </div>
+        </div>
+        <div id="rowSeparator"></div>
+        <div id="containerContatos" class="green-color">
+            <h4>informações de Contato:</h4>
+            <p><i class="fa-brands fa-instagram"></i> agrariatec</p> 
+            <p><i class="fa-brands fa-facebook"></i> Agrária Tec </p> 
+            <p><i class="fa-solid fa-envelope"></i> agraricatec@example.com </p> 
+        </div>
+    </div>
+    <div id="footerColor"></div>
 </template>
+ 
