140c29678f9b73eea75d3cf036854b3cf5a61a39
Caio Santos <caiofortesprofissional@gmail.com>
2024-10-26 12:04:21 -0300

    div separator

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,15 +1,19 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
 import Capa from './components/Capa.vue';
+import QuemSomos from './components/QuemSomos.vue';
+import Separator from './components/Separator.vue';
 import ProgramacaoEvento from './components/Programacao.vue';
 import Contato from './components/Contato.vue';
-import QuemSomos from './components/QuemSomos.vue';
 </script>
 
 <template>
   <!-- <TheHeader /> -->
   <Capa />
   <QuemSomos />
+  <Separator />
+  <QuemSomos />
+  <Separator />
   <!--  <ProgramacaoEvento />
   <Contato />  -->
 </template>
---
+++ project/src/components/Separator.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="containerSeparator">
+    <div class="separator"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Separator'
+}
+</script>
+
+<style scoped>
+#containerSeparator {
+  display: flex;
+  justify-content: center;
+
+}
+
+#containerSeparator div {
+  margin: 4rem 2rem;
+}
+
+.separator {
+  width: 87%;
+  height: 1px;
+  background-color: #80808047;
+  border-radius: 10px;
+}
+</style>
